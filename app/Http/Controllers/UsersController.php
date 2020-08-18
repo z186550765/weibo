e7ca2b4ac8dbbae50d97b28a179757f1304e26c8
ZhangHaiBao <2425107294@qq.com>
2020-08-18 13:58:20 +0000

    用户显示页面

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,11 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class UsersController extends Controller
 {
     public function create()
     {
         return view('users.create');
+    }
+
+    public function show(User $user)
+    {
+        // dump(compact('user'));
+        return view('users.show', compact('user'));
     }
 }
